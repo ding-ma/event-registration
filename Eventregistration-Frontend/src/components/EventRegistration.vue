1da68033bafbf61e8003eeec837040a3a5e6aab1
Ding Ma <43629633+ding-ma@users.noreply.github.com>
2021-11-12 13:50:46 -0500

    fetch person

---
+++ Eventregistration-Frontend/src/components/EventRegistration.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="eventregistration">
+    <h2>Persons</h2>
+    <table>
+      <tr v-for="person in persons">
+        <td>{{ person.name }}</td>
+        <td>
+          <ul>
+            <li v-for="event in person.events">
+              {{ event.name }}
+            </li>
+          </ul>
+        </td>
+      </tr>
+    <td>
+        <input type="text" v-model="newPerson" placeholder="Person Name">
+    </td>
+    <td>
+        <button v-bind:disabled="!newPerson" @click="createPerson(newPerson)">Create Person</button>
+    </td>
+    </tr>
+</table>
+  <span v-if="errorPerson" style="color:red">Error: {{errorPerson}} </span>
+  </div>
+</template>
+<script src="./registration.js">
+</script>
+<style>
+#eventregistration {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+  background: #f2ece8;
+}
+</style>
---
+++ Eventregistration-Frontend/src/components/registration.js
@@ -0,0 +1,71 @@
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+function PersonDto (name) {
+  this.name = name
+  this.events = []
+}
+
+function EventDto (name, date, start, end) {
+  this.name = name
+  this.eventDate = date
+  this.startTime = start
+  this.endTime = end
+}
+
+export default {
+  name: 'eventregistration',
+  data () {
+    return {
+      persons: [],
+      newPerson: '',
+      errorPerson: '',
+      response: []
+    }
+  },
+  created: function () {
+    // Initializing persons from backend
+    AXIOS.get('/persons')
+    .then(response => {
+      // JSON responses are automatically parsed.
+      this.persons = response.data
+    })
+    .catch(e => {
+      this.errorPerson = e
+    })
+    // Initializing events
+    AXIOS.get('/events')
+    .then(response => {
+      this.events = response.data
+    })
+    .catch(e => {
+      this.errorEvent = e
+      // this.errors.push(e)
+    })
+  },
+  methods: {
+    createPerson: function (personName) {
+      AXIOS.post('/persons/'.concat(personName), {}, {})
+        .then(response => {
+        // JSON responses are automatically parsed.
+          this.persons.push(response.data)
+          this.errorPerson = ''
+          this.newPerson = ''
+        })
+        .catch(e => {
+          var errorMsg = e.response.data.message
+          console.log(errorMsg)
+          this.errorPerson = errorMsg
+        })
+    }
+  }
+}
+
